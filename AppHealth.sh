d29f3c83575782cfd16788d44f003ee39431c79e
Haree <haresmart53@gmail.com>
2025-09-19 16:09:37 +0530

    commit

---
+++ AppHealth.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Website to check
+URL="https://www.google.com" # Url of the application
+
+# Ask the website for its status
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+
+# If the website is working
+if [ "$STATUS" -eq 200 ]; then
+  echo "✅ The application at $URL is UP (200 OK)"
+else
+  echo "❌ The application at $URL is DOWN (status $STATUS)"
+fi
