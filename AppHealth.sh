d5a669e4fa78b64609d5df2975bdca9bab9bcc10
Haree <haresmart53@gmail.com>
2025-09-19 16:10:11 +0530

    commit

--- AppHealth.sh
+++ AppHealth.sh
@@ -8,7 +8,7 @@ STATUS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
 
 # If the website is working
 if [ "$STATUS" -eq 200 ]; then
-  echo "✅ The application at $URL is UP (200 OK)"
+  echo "The application at $URL is UP (200 OK)"
 else
-  echo "❌ The application at $URL is DOWN (status $STATUS)"
+  echo "The application at $URL is DOWN (status $STATUS)"
 fi
